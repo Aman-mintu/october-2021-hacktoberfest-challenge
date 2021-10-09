808f31b79b185bde00087da608c3a1824c9c43d8
Aman Kumar <72150880+Aman-mintu@users.noreply.github.com>
2021-10-09 22:25:43 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr={11,22,33,44,55,66,77,88,99};
+        int target=77;
+        System.out.println(binarySearch(arr,target));
+    }
+    static int binarySearch(int[] arr,int target)
+    {
+        int start=0;
+        int end=arr.length-1;
+        while (start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]>target)   // from the left side of the middle element
+            {
+                end=mid-1;
+            }
+            else if(arr[mid]<target) //from the right side of the middle element
+            {
+                start=mid+1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
